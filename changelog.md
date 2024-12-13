4b517e5a52a25eb4a06791940c242b597000268c
Bruno Daniel <bdao.profissional@gmail.com>
2024-12-12 21:05:16 -0300

    exercicio typescript

--- changelog.md
+++ changelog.md
@@ -16,3 +16,4 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 -   Add arquivo orientacao_objeto.ts
 -   Add arquivo interface.ts
 -   Add arquivo dom.ts
+-   Add arquivo exercicio.ts
---
+++ src/exercicio.ts
@@ -0,0 +1,5 @@
+const multiplicar = (num1: number, num2: number): number => { return num1 * num2 }
+console.log(multiplicar(5, 2))
+
+const saudacao = (nome: string): string => { return `Olá ${nome}` }
+console.log(saudacao('Bruno Daniel'))
